b750aafe55c462ce443eb1dfbacdadeb14f3ea1b
Celeste Magisteel <celestemagisteel@gmail.com>
2019-11-20 15:30:35 +0000

    Implemented User Accounts, and set up password hashing with BCrypt

---
+++ src/main/java/xyz/fluxcore/AuctionHouse/entries/User.java
@@ -0,0 +1,37 @@
+package xyz.fluxcore.AuctionHouse.entries;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    public String username;
+    public String password;
+    public List<Integer> watchedLots;
+
+     public User() {}
+
+     public User(String username, String password) {
+         this.username = username;
+         this.password = hashPassword(password);
+         watchedLots = new ArrayList<>();
+     }
+
+     private String hashPassword(String password) {
+         return BCrypt.hashpw(password, BCrypt.gensalt());
+     }
+
+     public String getPassword() { return this.password; }
+
+     public boolean checkPassword(String password) {
+         return BCrypt.checkpw(password, this.password);
+     }
+
+     public void watchLot(int lotId) { if (!watchedLots.contains(lotId)) { watchedLots.add(lotId); }}
+
+     public void unwatchLot(int lotId) { if (watchedLots.contains(lotId)) { watchedLots.remove(lotId); }}
+
+     public List<Integer> getWatchedLots() { return this.watchedLots; }
+}
