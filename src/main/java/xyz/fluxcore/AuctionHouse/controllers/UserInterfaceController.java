1ba0366324c7849b07aabd18fe1adb4e96cac247
Kamg300 <Kamg1134@gmail.com>
2019-11-25 15:02:26 +0000

    Implemented a navigation menu

--- src/main/java/xyz/fluxcore/AuctionHouse/controllers/UserInterfaceController.java
+++ src/main/java/xyz/fluxcore/AuctionHouse/controllers/UserInterfaceController.java
@@ -1,6 +1,7 @@
 package xyz.fluxcore.AuctionHouse.controllers;
 
 import xyz.fluxcore.AuctionHouse.ui.actions.AuthenticateAction;
+import xyz.fluxcore.AuctionHouse.ui.actions.NavbarAction;
 import xyz.fluxcore.AuctionHouse.ui.views.LoginScreen;
 import xyz.fluxcore.AuctionHouse.ui.views.RegisterScreen;
 
@@ -10,10 +11,14 @@ public class UserInterfaceController {
 
     private AuctionHouseController auctionHouseController;
     private JFrame window;
+    private JMenuBar navbar;
 
     public UserInterfaceController(AuctionHouseController auctionHouseController) {
         this.auctionHouseController = auctionHouseController;
         window = new JFrame();
+        navbar = new JMenuBar();
+        initializeNavbar();
+        window.setJMenuBar(navbar);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setTitle("Welcome to my Auction House!");
         window.setSize(400, 400);
@@ -33,6 +38,27 @@ public class UserInterfaceController {
         RegisterScreen registerScreen = new RegisterScreen(new AuthenticateAction(auctionHouseController));
         window.setContentPane(registerScreen.getPanel());
         window.setVisible(true);
+    }
+
+    private void initializeNavbar() {
+        // Auth Menu
+        JMenu authMenu = new JMenu("Authentication");
+
+        // Login Button
+        NavbarAction navbarAction = new NavbarAction(this);
+        JMenuItem loginButton = new JMenuItem("Login");
+        loginButton.addActionListener(navbarAction);
+        loginButton.setActionCommand("login");
+        authMenu.add(loginButton);
+
+        // Login Button
+        JMenuItem registerButton = new JMenuItem("Register");
+        registerButton.addActionListener(navbarAction);
+        registerButton.setActionCommand("register");
+        authMenu.add(registerButton);
+
+        navbar.add(authMenu);
+
     }
 
     private void clearScreen() {
