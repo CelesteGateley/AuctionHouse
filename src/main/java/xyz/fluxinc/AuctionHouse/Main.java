512956811861709d1c3f1d0f43c5c0fe42eade50
Kamg300 <Kamg1134@gmail.com>
2019-12-09 15:00:51 +0000

    Changes to Main.java

--- src/main/java/xyz/fluxinc/AuctionHouse/Main.java
+++ src/main/java/xyz/fluxinc/AuctionHouse/Main.java
@@ -1,17 +1,11 @@
 package xyz.fluxinc.AuctionHouse;
 
-import net.jini.core.entry.Entry;
-import xyz.fluxinc.AuctionHouse.controllers.AuctionHouseController;
 import xyz.fluxinc.AuctionHouse.controllers.SystemController;
-import xyz.fluxinc.AuctionHouse.entries.Auction1755082;
-import xyz.fluxinc.AuctionHouse.entries.AuctionHouse1755082;
-import xyz.fluxinc.AuctionHouse.entries.AuctionHouseLock1755082;
 import xyz.fluxinc.AuctionHouse.exceptions.space.SpaceException;
 import xyz.fluxinc.AuctionHouse.exceptions.space.SpaceNotFoundException;
 import xyz.fluxinc.AuctionHouse.exceptions.authentication.UserExistsException;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 
@@ -30,15 +24,7 @@ public class Main {
             catch(UserExistsException ignored) {}
         }
 
-        //systemController.getUserInterfaceController().showLoginScreen();
-
-        Auction1755082 auction = systemController.getAuctionHouseController().placeAuction("Test", 1);
-        AuctionHouse1755082 haus = systemController.getAuctionHouseController().getAuctionHouse();
-        System.out.println(haus.currentCount);
-
-        System.out.println(auction.name);
-        System.out.println("Finished Processing...");
-        System.exit(0);
+        systemController.getUserInterfaceController().showLoginScreen();
     }
 
     private static String getHostName(List<String> arguments) {
