aeb914c7f6448c071edc15fc19f28fd3ff3627f1
CelesteMagisteel <celestemagisteel@gmail.com#>
2019-12-11 21:44:54 +0000

    Added a help menu

--- src/main/java/xyz/fluxinc/AuctionHouse/Main.java
+++ src/main/java/xyz/fluxinc/AuctionHouse/Main.java
@@ -20,33 +20,36 @@ import java.util.List;
 public class Main {
     //-Djava.security.policy=policy.all -Djava.rmi.server.useCodebaseOnly=false
 
-    public static void main(String[] args) throws SpaceNotFoundException, SpaceException, UserExistsException, UserNotFoundException, AuthenticationException, AuctionNotFoundException {
+    private static final String DEFAULT_HOST = "localhost";
+
+    public static void main(String[] args) throws SpaceNotFoundException, SpaceException, UserNotFoundException, AuthenticationException, AuctionNotFoundException {
         List<String> arguments = Arrays.asList(args);
         boolean addRoot = arguments.contains("-add-root");
         boolean anonymous = arguments.contains("-anonymous");
+        boolean help = arguments.contains("-help");
         String hostname = getHostName(arguments);
-        SpaceController spTemp = new SpaceController(hostname);
-        spTemp.takeAll(new AuctionHouse());
-        spTemp = null;
 
-        SystemController systemController = new SystemController(hostname);
-        if (addRoot) {
-            try { systemController.getAuthenticationController().registerAdministrator("root", "root"); }
-            catch(UserExistsException ignored) {}
+        if (help) {
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+            System.out.println("|                                           Welcome to the Auction House System!                                          |");
+            System.out.println("|                                   This system was developed by u1755082 Kieran Gateley                                  |");
+            System.out.println("|                        This system has a few arguments to make running and using the system easier                      |");
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+            System.out.println("| -anonymous - Runs the system without logging in. No changes can be made but the system can be viewed                    |");
+            System.out.println("| -add-root - Adds the root administrator account (root, root) which can edit the entire system                           |");
+            System.out.println("| -host (hostname) - Sets the hostname for the system to connect to. If this isn't set, then " + DEFAULT_HOST + " is used            |");
+            System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        } else {
+            SystemController systemController = new SystemController(hostname);
+
+            if (addRoot) {
+                try { systemController.getAuthenticationController().registerAdministrator("root", "root"); }
+                catch(UserExistsException ignored) {}
+            }
         }
 
-        systemController.getAuthenticationController().login("root", "root");
 
-        systemController.getAuctionHouseController().placeAuction("Test 1", 1, 1);
 
-        for (int i = 0; i < 10; i++) {
-            systemController.getAuctionHouseController().placeBid(0, i);
-        }
-
-        List<Bid> bids = systemController.getAuctionHouseController().getBids(0);
-        for (Bid bid : bids) {
-            System.out.println(bid.bidAmount);
-        }
 
         System.exit(0);
 
@@ -58,10 +61,10 @@ public class Main {
             try {
                 return arguments.get(index + 1);
             } catch (IndexOutOfBoundsException e) {
-                return "localhost";
+                return DEFAULT_HOST;
             }
         } else {
-            return "localhost";
+            return DEFAULT_HOST;
         }
     }
 
