8f594ef8461c66fa59156decc9bf17039557779f
Celeste Magisteel <kamg1134@gmail.com>
2019-12-19 16:27:00 +0000

    Implemented new navbar actions

--- src/main/java/xyz/fluxinc/auctionhouse/ui/actions/NavbarAction.java
+++ src/main/java/xyz/fluxinc/auctionhouse/ui/actions/NavbarAction.java
@@ -1,17 +1,22 @@
 package xyz.fluxinc.auctionhouse.ui.actions;
 
+import xyz.fluxinc.auctionhouse.controllers.AuthenticationController;
 import xyz.fluxinc.auctionhouse.controllers.UserInterfaceController;
+import xyz.fluxinc.auctionhouse.exceptions.authentication.AuthenticationException;
 import xyz.fluxinc.auctionhouse.exceptions.space.SpaceException;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class NavbarAction implements ActionListener {
 
+    private AuthenticationController authenticationController;
     private UserInterfaceController uiController;
 
-    public NavbarAction(UserInterfaceController uiController) {
+    public NavbarAction(UserInterfaceController uiController, AuthenticationController authenticationController) {
         this.uiController = uiController;
+        this.authenticationController = authenticationController;
     }
 
     @Override
@@ -33,6 +38,37 @@ public class NavbarAction implements ActionListener {
                 break;
             case "place-auction":
                 uiController.showMakeAuction();
+                break;
+            case "change-contact-info":
+                String contactInfo = JOptionPane.showInputDialog(uiController.getWindow(), "Enter the new contact information");
+                if (contactInfo.isEmpty()) {
+                    JOptionPane.showMessageDialog(uiController.getWindow(), "You must have contact information");
+                    break;
+                }
+                try {
+                    authenticationController.changeContactInfo(contactInfo);
+                } catch (SpaceException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "change-password":
+                String oldPassword = JOptionPane.showInputDialog(uiController.getWindow(), "Enter your current password");
+                String newPassword = JOptionPane.showInputDialog(uiController.getWindow(), "Enter your new password");
+                String confirmPassword = JOptionPane.showInputDialog(uiController.getWindow(), "Re-enter your new password");
+
+                if (oldPassword.isEmpty() || newPassword.isEmpty() || confirmPassword.isEmpty()) {
+                    JOptionPane.showMessageDialog(uiController.getWindow(), "You must enter something into each box");
+                }
+                if (!newPassword.equals(confirmPassword)) {
+                    JOptionPane.showMessageDialog(uiController.getWindow(), "Passwords do not match");
+                }
+
+                try {
+                    authenticationController.changePassword(oldPassword, newPassword);
+                } catch (AuthenticationException ex) {
+                    JOptionPane.showMessageDialog(uiController.getWindow(), "Incorrect Password Entered");
+                } catch (SpaceException ignored) { }
+
             default:
                 break;
         }
